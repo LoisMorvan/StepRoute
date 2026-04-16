c97a29937ff95760ef575b3962ebc09e6d5ab42a
Loïs Morvan <lois.morvan@laposte.net>
2026-04-16 15:36:38 +0200

    chore: bump build version to 1.2.3

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ const routeApiUrl =
 const config: ExpoConfig = {
   name: 'StepRoute',
   slug: 'steproute',
-  version: '1.0.0',
+  version: '1.2.3',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
@@ -24,7 +24,7 @@ const config: ExpoConfig = {
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#ffffff',
     },
-    versionCode: 3,
+    versionCode: 4,
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     package: 'com.loismorvan.steproute',
